78fb9d9d61f9ad7f78ee56e4c007924bff82f81e
Artur do Gera <artur.souza@liferay.com>
2021-07-21 17:10:58 -0300

    add exercicios

---
+++ Artur/arq.py
@@ -0,0 +1,6 @@
+opcao=int(input('escolha uma opcao abaixo: \n 1 - cachorro quente '))
+cachorro=4.00
+quant=1
+if opcao ==1:
+    quant=int(input('Quantos voce quer? '))
+    print('o total eh: ',quant*cachorro)
