8039e75dc630668d312304c50ae03df0ff5804a7
Igor Gouveia <igor.gouveia@liferay.com>
2021-07-26 16:52:55 -0300

    adicionando estudos

---
+++ igao_estudos/testejs/aula21/index.js
@@ -0,0 +1,20 @@
+/*
+Operadores de comparacao
+> maior que
+>= maior que ou igual a 
+< menor que 
+<= menor que ou igual a 
+== igualdade (valor) ******* 
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) **********
+!== diferente estrito (valor e tipo)
+*/
+
+// const comp = 10 > 5;
+// console.log(comp);
+
+const num1 = 10;  //number
+const num2 = 10; //number
+
+const comp = num1 !== num2;
+console.log(comp);
---
+++ igao_estudos/testejs/aula22/index.js
@@ -0,0 +1,19 @@
+/*
+Operadores Logicos
+&& -> AND -> E  // Todas as expressoes precisam ser verdadeiras para retornar true
+|| -> Or -> OU   // Se tiver um true, a funcao sera true. Se for toda false, a funcao sera false.
+! -> NOT -> NAO
+*/
+
+// const expressaoAnd = true && true && true && true;
+// const expressaoOr = false || false || true || false;    
+// console.log(expressaoOr);
+
+
+// const usuario = 'Luiz';  // form usuario digitou
+// const senha = '123456'; // form usuario digitou
+// //                    True                    False;
+// const vaiLogar = usuario === 'Luiz' && senha === '123456';
+// console.log(vaiLogar) ;
+
+// console.log(!true) \\ console.log(!!true);
