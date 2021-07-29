e5aef64f1db4cafaaeea5ec3f9c78bf7654b4f47
Artur do Gera <artur.souza@liferay.com>
2021-07-29 16:54:31 -0300

    exercicio de media

--- Artur/aula.js
+++ Artur/aula.js
@@ -4,5 +4,20 @@
         //var imc = Number(((altura**2)*peso)/10).toFixed(1)
         //Number(window.alert(`seu imc eh: ${imc}`))
 
-        const nome = artur
-        document.body.innerHTML+=`a`
+       /* const nome = artur
+        document.body.innerHTML+=`a` */
+
+
+var num1=Number(prompt("Digite um numero: "))
+
+const SP = ['a']
+switch (num1){
+        case 11:
+                document.body.innerHTML = 'SP'
+        break
+                
+                
+        default:
+                document.body.innerHTML = 'nao eh um ddd de um estado brasileiro'
+        break
+} 
